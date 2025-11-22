b1f1ddab5b1d63a0b1dc3384e8fe3d4cd5c30bfb
magniklo <magniklom@gmail.com>
2025-11-22 12:22:32 +0300

    first commit

--- src/main/java/ru/itis503/infa/Animal.java
+++ src/main/java/ru/itis503/infa/Animal.java
@@ -1,4 +1,11 @@
 package ru.itis503.infa;
 
 public class Animal {
+        String name;
+        int x;
+        int y;
+
+        public void move() {
+
+        }
 }
--- src/main/java/ru/itis503/infa/Main.java
+++ src/main/java/ru/itis503/infa/Main.java
@@ -1,4 +1,7 @@
 package ru.itis503.infa;
 
 public class Main {
+    public static void main(String[] args) {
+
+    }
 }
